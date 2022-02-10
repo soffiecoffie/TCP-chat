f5ad65bcfe9c1f98298d2f7898724f58b52db56b
Sofia Hristova <63425040+soffiecoffie@users.noreply.github.com>
2022-02-10 15:37:07 +0200

    Update main.go

--- client/main.go
+++ client/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
 	"io"
 	"net"
 	"os"
-	"strings"
 )
+
+//You can add cmd to say number of users and all usernames and private message to user by saying "!private /username/ /message/"
+//check if server terminates(ctrl+c) badly and kicks you out
 
 // Reads from Server and returns the message as a string instead of buffer
 func readFromCon(c net.Conn) string {
@@ -27,21 +28,12 @@ func readFromCon(c net.Conn) string {
 	s := bytes.Trim(buf, "\x00")
 
 	return string(s)
-	// return strings.Join(s, " ")
 }
 
-// Writes to server from standard input and returns the sent message
-func writeToServer(c net.Conn) string {
-	reader := bufio.NewReader(os.Stdin)
-	message, _ := reader.ReadString('\n') //returns err!=nil only when it doesnt end in \n
-	// Remove '\n' from the message and the ascii 13 character that breaks everything.. 
-	message = strings.TrimSuffix(message, "\n")
-	message = strings.TrimSuffix(message, string(13))
+// Writes to server from a given string and returns the sent message
+func writeToServerStr(c net.Conn, message string) string {
 	// Input the message in buf
 	buf := []byte(message)
-	fmt.Println("length of usr ", len(message))
-	fmt.Println("length of buf ", len("toast"))
-	fmt.Println(message)
 	// Write to server
 	_, err := c.Write(buf)
 	if err != nil {
@@ -49,16 +41,35 @@ func writeToServer(c net.Conn) string {
 		// 	fmt.Println("Error with reading: ", err.Error())
 		// }
 		// break
-		// return //y/n
+		// return //yes or no
 		os.Exit(1)
 	}
 	return message
 }
 
+// Writes to server from standard input and returns the sent message
+func writeToServer(c net.Conn) string {
+	var message string
+	fmt.Scanln(&message)
+	return writeToServerStr(c, message)
+}
+
+// // Writes to the chat from standard input and returns the sent message
+// func talkToChat(c net.Conn, usr string) string {
+// 	var message string
+// 	fmt.Scanf("%s", &message)
+// 	return writeToServerStr(c, usr+": "+message)
+// }
+
 func main() {
+	// fmt.Print("Input the server you want to connect to: ")
+	// var addr string
+	// fmt.Scanf("%s", &addr)
+	// client, err := net.Dial("tcp", addr)
+
 	// client, err := net.Dial("tcp", "192.168.0.3:8080")
 	// client, err := net.Dial("tcp", "192.168.0.3:0")
-	client, err := net.Dial("tcp", "192.168.0.3:52429")
+	client, err := net.Dial("tcp", "192.168.0.3:64619")
 	if err != nil {
 		fmt.Println("Error with connecting: ", err.Error())
 		return
@@ -81,11 +92,35 @@ func main() {
 		again = received == "again"
 	}
 	username := received
-	fmt.Println("Username is: " + username)
+	// fmt.Println("Username is: " + username)
 
-	// for {
-	// 	received = readFromCon(client)
-	// 	fmt.Print(received)
-	// }
+	received = readFromCon(client)
+	fmt.Print(received)
 
+	cmd := "!exit"
+	run := true
+
+	// Receiving chat messages
+	go func() {
+		for run {
+			received = readFromCon(client)
+			fmt.Println(received)
+		}
+	}()
+
+	//TURNS OUT SCANLN doesnt scan the whole line and ignores ALL whitespaces so use bufio to replace it
+	for run {
+		// Talking in the chat
+		var message string
+		fmt.Scanln(&message)
+		fmt.Println("YOUR MESSAGE IS:", message)
+		if message == cmd {
+			run = false
+			writeToServerStr(client, username+" exited.")
+			writeToServerStr(client, "!exit")
+			fmt.Println("Sad to see you go!")
+		} else {
+			writeToServerStr(client, username+": "+message)
+		}
+	}
 }
