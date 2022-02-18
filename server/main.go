02b7951aae2c9194e3051f9def2579f7021af5d8
Sofia Hristova <63425040+soffiecoffie@users.noreply.github.com>
2022-02-18 02:24:49 +0200

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
 	"net"
+	"os"
+	"os/signal"
 	"strings"
 	"sync"
+	"syscall"
+)
+
+var (
+	run             = true
+	sendingMessages = true
+	ok              = false
+	cleaningHistory = false
+	inChat          = false
+	chat            ConcurrentSlice
+	ind             ConcSliceIndices
 )
 
 type ConcurrentSlice struct {
@@ -14,27 +28,38 @@ type ConcurrentSlice struct {
 	messages []string
 }
 
-func (slice *ConcurrentSlice) Append(str string) {
-	slice.messages = append(slice.messages, str)
-}
-
-func (slice *ConcurrentSlice) CleanHistory(ind int) {
+// Appends string to the messages of a ConcurrentSlice
+func (slice *ConcurrentSlice) Append(str string) int {
 	slice.Lock()
 	defer slice.Unlock()
 
-	slice.messages = slice.messages[ind:]
+	slice.messages = append(slice.messages, str)
+	return len(slice.messages) - 1
 }
 
-///////////////////////////////
+// Removes the last ind messages from the messages of a ConcurrentSlice
+func (slice *ConcurrentSlice) CleanHistory(ind int) {
+	slice.Lock()
+	defer slice.Unlock()
+	cleaningHistory = true
+	slice.messages = slice.messages[ind:]
+	cleaningHistory = false
+}
+
 type ConcSliceUsr struct {
 	sync.Mutex
 	usernames []string
 }
 
+// Append a username to the usernames slice of a ConcSliceUser
 func (slice *ConcSliceUsr) Append(usr string) {
+	slice.Lock()
+	defer slice.Unlock()
+
 	slice.usernames = append(slice.usernames, usr)
 }
 
+// Removes username from usernames slice of a ConcSliceUser
 func (slice *ConcSliceUsr) RemoveUsername(str string) {
 	slice.Lock()
 	defer slice.Unlock()
@@ -47,19 +72,20 @@ func (slice *ConcSliceUsr) RemoveUsername(str string) {
 	}
 }
 
-///////////////////////////////
 type ConcSliceIndices struct {
 	sync.Mutex
 	indices []*int
 }
 
+// Appends an index to the indices slice of a ConcSliceIndices
 func (slice *ConcSliceIndices) Append(ind *int) {
+	slice.Lock()
+	defer slice.Unlock()
+
 	slice.indices = append(slice.indices, ind)
 }
 
-func (slice *ConcSliceIndices) GetInd(ind int) *int {
-	return slice.indices[ind]
-}
+// Removes index from the indices slice of a ConcSliceIndices
 func (slice *ConcSliceIndices) RemoveInd(ind *int) {
 	slice.Lock()
 	defer slice.Unlock()
@@ -77,35 +103,32 @@ func readFromCon(c net.Conn) string {
 	buf := make([]byte, 1024)
 	_, err := c.Read(buf)
 	if err != nil {
+		if !run {
+			os.Exit(0)
+		}
 		if err != io.EOF {
 			fmt.Println("Error with reading: ", err.Error())
+			return ""
 		}
-		// break
-		// return
-		//os.Exit(1)
 		fmt.Println("err = io.eof")
+		return ""
 	}
 
 	// Removes null characters from buf and puts result in slice
 	s := bytes.Trim(buf, "\x00")
 
 	return string(s)
-	// return strings.Join(s, " ")
 }
 
-// Writes a given string to a given conn and returns the sent message
+// Writes a given string to a given connection and returns the sent message
 func writeFromStr(c net.Conn, message string) string {
 	// Input the message in buf
 	buf := []byte(message)
 	// Write to server
 	_, err := c.Write(buf)
 	if err != nil {
-		// if err != io.EOF {
 		fmt.Println("Error with writing: ", err.Error())
-		// }
-		// break
-		// return //yes or no
-		// os.Exit(1)
+		return ""
 	}
 	return message
 }
@@ -123,22 +146,21 @@ func contains(s []string, str string) bool {
 
 // Checks if str has whitespaces
 func hasWhitespaces(str string) bool {
-	for i := 0; i < len(str); i++ {
-		if str[i] == ' ' || str[i] == '\t' || str[i] == '\n' {
+	for _, v := range str {
+		if v == ' ' || v == '\t' || v == '\n' {
 			return true
 		}
 	}
 	return false
 }
 
-// should i forbid symbols that aren't letters or numbers? i dont like usernames like ____-
 // Checks if there are "forbidden" symbols in a given string
 func hasForbiddenSymbols(str string) bool {
 	// allowed symbols: A-Z, a-z, ., -, _, ~
-	for i := 0; i < len(str); i++ {
-		if !((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122) ||
-			(str[i] >= 48 && str[i] <= 57) || str[i] == '-' || str[i] == '.' ||
-			str[i] == '_' || str[i] == '~') {
+	for _, v := range str {
+		if !((v >= 65 && v <= 90) || (v >= 97 && v <= 122) ||
+			(v >= 48 && v <= 57) || v == '-' || v == '.' ||
+			v == '_' || v == '~') {
 			return true
 		}
 	}
@@ -169,59 +191,160 @@ func valid(str string) bool {
 	return len(str) > 1 && !hasWhitespaces(str) && !hasForbiddenSymbols(str)
 }
 
-// find a way to check if user leaves with ctrl+C so it doesn't break my server
+// Handles signals
+func handleSignals(chatting bool) {
+	// Channel to read signals.
+	sigs := make(chan os.Signal, 1)
+	fmt.Println("IN HANDLE")
+	// Registers the given channel to receive notifications of the specified signals.
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	stop := false
+
+	for !stop {
+		sig := <-sigs
+		if chatting != inChat {
+			return
+		}
+		switch sig {
+		case syscall.SIGHUP:
+			fmt.Println("SIGHUP")
+			if !chatting {
+				// closingServer(chat)
+				closingServer()
+				stop = true
+			} else {
+				os.Exit(0)
+			}
+			//os.Exit()??
+			// chat.Append("Server has closed!\n")
+			// fmt.Println("Server closed!")
+		case syscall.SIGINT:
+			fmt.Println("SIGINT")
+			if !chatting {
+				// closingServer(chat)
+				closingServer()
+				stop = true
+			} else {
+				os.Exit(0)
+			} // chat.Append("Server has closed!\n")
+			// fmt.Println("Server closed!")
+		case syscall.SIGTERM:
+			fmt.Println("SIGTERM")
+			if !chatting {
+				// closingServer(chat)
+				closingServer()
+				stop = true
+			} else {
+				os.Exit(0)
+			} // chat.Append("Server has closed!\n")
+			// fmt.Println("Server closed!")
+		case syscall.SIGQUIT:
+			fmt.Println("SIGQUIT")
+			if !chatting {
+				// closingServer(chat)
+				closingServer()
+				stop = true
+			} else {
+				os.Exit(0)
+			} // chat.Append("Server has closed!\n")
+			// fmt.Println("Server closed!")
+		default:
+			fmt.Println("Unknown signal")
+		}
+	}
+}
+
+// Closes server
+func closingServer() {
+	fmt.Println("Stopping server...")
+	run = false
+
+	chat.Append("Chat is closing now! See you next time!")
+	if !ok {
+		checkOk()
+	}
+	sendingMessages = false
+	fmt.Println("Server has closed!")
+}
+
+// Reads from standard input and returns the message
+func readFromStdin() string {
+	var reader = bufio.NewReader(os.Stdin)
+	message, _ := reader.ReadString('\n')
+
+	if len(message) > 2 {
+		// Removes last 2 characters which are '\n' and ascii(13) ""
+		message = message[:len(message)-2]
+	} else if len(message) == 1 && message[0] == '\n' ||
+		len(message) == 2 && message[0] == 13 && message[1] == '\n' {
+		return ""
+	}
+	return message
+}
+
+// Checks if all messages have been read
+func checkOk() {
+	allread := false
+	// Until every client reads all messages
+	for !allread {
+		allread = true
+		for i := 0; i < len(ind.indices); i++ {
+			if *ind.indices[i] < len(chat.messages) {
+				allread = false
+			}
+		}
+	}
+	ok = true
+}
 
 func main() {
-	// fmt.Print("Input your local ip address (e.g. 192.168.0.3) to create a server: ")
-	// var host string
-	// fmt.Scanf("%s", &host)
-	// listener, err := net.Listen("tcp", host+":")
+	go handleSignals(false)
 
-	//For TCP networks, if the host in the address parameter is empty Listen listens on all available unicast and anycast IP addresses of the local system.  The address can use a host name, but this is not recommended, because it will create a listener for at most one of the host's IP addresses.
-	// listener, err := net.Listen("tcp", ":8080")
-	// listener, err := net.Listen("tcp", "192.168.0.3:8080")
-	// you need to write ur own address
-	listener, err := net.Listen("tcp", "192.168.0.3:")
+	fmt.Print("Input your local ip address (e.g. 192.168.0.3) to create a server: ")
+	var host string
+	fmt.Scanf("%s", &host)
+	listener, err := net.Listen("tcp", host+":")
+
 	if err != nil {
 		fmt.Println("Error with listening: ", err.Error())
-		return
-		//os.Exit(1)
+		os.Exit(1)
 	}
 	defer listener.Close()
+
+	ok = true
+
 	fmt.Println("Server Address: " + listener.Addr().String())
 
 	// creating an array of existing usernames
 	var users ConcSliceUsr
 
-	// chat messages
-	var chat ConcurrentSlice
-	var indices ConcSliceIndices
-	run := true
+	inChat = true
+	go handleSignals(true)
 
 	go func() {
 		for run {
 			if len(chat.messages) >= 256 {
 				var allread bool
-				//lock w
-				for i := 0; i < len(indices.indices); i++ {
-					if *(indices.indices)[i] < 256 { //whats tha val of *indices.indices[i]
+				for i := 0; i < len(ind.indices); i++ {
+					if *(ind.indices)[i] < 255 {
 						allread = false
+						fmt.Println("INSIDE HISTORY CLEAN! ")
 					}
 				}
 				// Until everyone reads the last 256 messages
 				for allread {
 					allread = true
-					for i := 0; i < len(indices.indices); i++ {
-						if *indices.indices[i] < 256 {
+					for i := 0; i < len(ind.indices); i++ {
+						if *ind.indices[i] < 255 {
 							allread = false
 						}
 					}
 				}
 
 				chat.CleanHistory(256)
-				for i := 0; i < len(indices.indices); i++ {
-					*indices.indices[i] -= 256
-					//unlock w
+				for i := 0; i < len(ind.indices); i++ {
+					*ind.indices[i] -= 255
 				}
 			}
 		}
@@ -231,32 +354,44 @@ func main() {
 		for run {
 			conn, err := listener.Accept()
 			if err != nil {
-				fmt.Println("Error with listening: ", err.Error())
-				//os.Exit(1)
+				if run {
+					fmt.Println("Error with listening: ", err.Error())
+				} else {
+					return
+				}
 			}
-			// fmt.Println("Local addr: " + conn.LocalAddr().String())  // this is mine aka the server
-			// fmt.Println("Remote addr: " + conn.RemoteAddr().String()) //this is the clients
+			ok = false
 
-			// Handling the connection
 			go func(conn net.Conn) {
 				fmt.Println("Remote address " + conn.RemoteAddr().String() + " connected!")
 
-				// conn.Write([]byte("Welcome to the server! Pick a nickname: "))
-				// conn.Write([]byte("Welcome to the server! What should we call you: "))
-
-				//Asking the client to create a username
+				// Asking the client to create a username
 				conn.Write([]byte("Welcome to the server! Create a username: "))
 
-				// username = string(buf[:])
+				defer conn.Close()
 				username := readFromCon(conn)
-				//Removes the whitespaces from the username if there are any
+
+				// Removes the whitespaces from the username if there are any
 				username = removeWhitespaces(username)
-				// fmt.Println("TEMP picked username: -" + username + "-")
+				if username == "!exit" {
+					fmt.Println(conn.RemoteAddr().String() + " exited.")
+					// Telling client it's ok to exit
+					writeFromStr(conn, "!exit")
+					return
+				}
 
 				again := contains(users.usernames, username) || !valid(username)
+
 				//Loop to let the client create a username that is valid and not already in use
 				for again {
+					if username == "!exit" {
+						fmt.Println(conn.RemoteAddr().String() + " exited.")
+						conn.Close()
+						return
+					}
+
 					conn.Write([]byte("again"))
+
 					if contains(users.usernames, username) {
 						conn.Write([]byte("Uh oh! The username you chose is already taken! Enter another username: "))
 					}
@@ -266,27 +401,28 @@ func main() {
 
 					username = removeWhitespaces(readFromCon(conn))
 					again = contains(users.usernames, username) || !valid(username)
+
 				}
 				conn.Write([]byte(username))
 
 				//Adding the chosen username to the slice of usernames
 				users.Append(username)
 
-				//TEMP remember to make client read this
 				conn.Write([]byte("You have successfully connected to the server! To leave just type \"!exit\"\n"))
 
-				// Adding announcement to the chat messages
-				chat.Append(username + " joined the chat! Say hi!\n")
-
 				// Index to start reading messages from
-				ind := len(chat.messages) - 1
+				readInd := chat.Append(username + " joined the chat! Say hi!\n")
+				ind.Append(&readInd)
 
+				sendMsgToCon := true
+				// The index to the last message that needs to be read
+				var lastInd int
 				// Updating chat in go routine
 				go func() {
-					for run {
-						if ind < len(chat.messages) {
-							writeFromStr(conn, chat.messages[ind])
-							ind++
+					for sendingMessages && sendMsgToCon {
+						for readInd < len(chat.messages) {
+							writeFromStr(conn, chat.messages[readInd])
+							readInd++
 						}
 					}
 				}()
@@ -295,26 +431,41 @@ func main() {
 				for run {
 					receive := readFromCon(conn)
 					if receive == "!exit" {
-						chat.Append(username + " disconnected.")
+						sendMsgToCon = false
+						lastInd = chat.Append(username + " disconnected.")
+						if cleaningHistory {
+							lastInd -= 255
+						}
+
+						// Assuring all messages up to the one above will be read
+						for readInd <= lastInd {
+							writeFromStr(conn, chat.messages[readInd])
+							readInd++
+						}
+
+						// Telling client it's ok to exit
+						writeFromStr(conn, "!exit")
 						users.RemoveUsername(username)
-						indices.RemoveInd(&ind)
-						conn.Close()
-						break
+						ind.RemoveInd(&readInd)
+						return
 					} else {
 						// Adding new message to the chat messages slice
 						chat.Append(receive)
 					}
 				}
-
 			}(conn)
 		}
 	}()
 
 	var cmd string
-	for {
-		fmt.Scanf("%s", cmd)
+	for run {
+		cmd = readFromStdin()
 		if cmd == "stop" {
-			run = false
+			if len(users.usernames) == 0 {
+				ok = true
+			}
+			closingServer()
+			return
 		}
 	}
 }
